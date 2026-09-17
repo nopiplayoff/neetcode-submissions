add732de13c3cbd0977a2f17d6bdb625dc20b74a
xmeng <xianglongmg@gmail.com>
2026-09-16 23:46:19 -0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq = defaultdict(int)
+        left, right = 0, 0
+        replace = 0
+        longest = 0
+
+        while right < len(s):
+            s_right = s[right]
+
+            if freq[s_right] < max(freq.values()):
+                replace += 1
+            
+            freq[s_right] += 1
+            right += 1
+            
+            while replace > k:
+                s_left = s[left]
+                freq[s_left] -= 1
+
+                if freq[s_left] < max(freq.values()):
+                    replace -= 1
+
+                left += 1
+            
+            longest = max(longest, right - left)
+
+        return longest
+            
+    # def get_most_frequent_char(freq: map) -> tuple[str, int]:
+    #     ch = 0
+    #     f = freq[ch]
+
+    #     for i in range(1, 26):
+    #         if freq[i] > f:
+    #             f = freq[i]
+    #             ch = i
+        
+    #     return chr(ch + ord("a")), f
